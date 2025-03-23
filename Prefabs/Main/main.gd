5ae0949d51654933eaedcd599981bafb4737fb4a
Saxon267 <shaosaxon@gmail.com>
2025-03-23 14:15:22 +0800

    04_Main关卡

---
+++ Prefabs/Main/main.gd
@@ -0,0 +1,45 @@
+extends Node
+
+@export var mob_scene:PackedScene#加载Mob
+var score
+
+
+func _ready() -> void:
+	pass
+
+func _on_player_hit() -> void:
+	pass
+	
+func game_over():
+	$Timer_ScoreTimer.stop()
+	$Timer_Mob.stop()
+	
+func new_game():
+	score=0
+	$Player.start($StartPosition.position)
+	$Timer_StartTimer.start()
+
+
+func _on_timer_mob_timeout() -> void:
+	var mob=mob_scene.instantiate()
+	var mob_spawn_location=$Mob_Path/Mob_Spawn_Location
+	mob_spawn_location.progress_ratio=randf()#在路径上的随机位置生成敌人
+	mob.position=mob_spawn_location.position
+	
+	var direction=mob_spawn_location.rotation+PI/2
+	direction+=randf_range(-PI/4,PI/4)
+	mob.rotation=direction
+	
+	var velocity=Vector2(randf_range(150.0,250.0),0.0)
+	mob.linear_velocity=velocity.rotated(direction)
+	
+	add_child(mob)
+
+
+func _on_timer_score_timer_timeout() -> void:
+	score+=1
+
+
+func _on_timer_start_timer_timeout() -> void:
+	$Timer_Mob.start()
+	$Timer_ScoreTimer.start()
